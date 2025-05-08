75d85b0d94676f9cadc650835236218ad11dd0b1
Adrii-0906 <adrianranaescote4@gmail.com>
2025-05-08 14:23:31 +0200

    Repaso Ficheros

--- JAVA/RepasoFicheros/FicherosIO/Ejercicio7.java
+++ JAVA/RepasoFicheros/FicherosIO/Ejercicio7.java
@@ -1,10 +1,39 @@
 package FicherosIO;
 
+import java.awt.*;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class Ejercicio7 {
     // Escribe un programa que busque una palabra específica dentro del archivo datos.txt e indique cuántas veces aparece.
 
 
     public static void main(String[] args) {
+        String palabraBuscada = "me";
+        boolean encontrada = false;
 
+        try {
+            BufferedReader br = new BufferedReader(new FileReader("/home/adrian/Documentos/1DAM/Programación/JAVA/RepasoFicheros/FicherosIO/datos.txt"));
+            String linea;
+            int numLine = 1;
+
+            while ((linea = br.readLine()) != null) {
+                if (linea.contains(palabraBuscada)) {
+                    System.out.println("Palabra encontrada en la linea " + numLine + ": " + linea);
+                    encontrada = true;
+                }
+                numLine++;
+            }
+
+            br.close();
+
+            if (!encontrada) {
+                System.out.println("La palabra no ha sido encontrada");
+            }
+
+        } catch (IOException e) {
+            System.out.println("Error: " + e.getMessage());
+        }
     }
 }
